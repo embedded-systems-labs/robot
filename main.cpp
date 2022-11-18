73a6a2c5da9541cf9e7ace9c895085d04141963e
Alex McGarity <amcgarity123@gmail.com>
2022-11-18 14:43:02 -0600

    improved readability

--- main.cpp
+++ main.cpp
@@ -1,91 +1,200 @@
 #include "mbed.h"
-DigitalOut out1(D7),out2(D6),
-out3(D5),out4(D4);
-PwmOut ena(D9),enb(D3);
+
+//the wheel speed
+const float wheelSpeed = 0.1;
+
+// digital outputs for the wheels
+DigitalOut rightFwdDrive(D7), leftBackDrive(D6),
+    leftFwdDrive(D5), leftBackDrive(D4);
+rightFwdDrive = leftBackDrive = leftFwdDrive = leftBackDrive = 0;
+// digital outputs for the Pulse Width Modulation controlling wheel speed
+PwmOut rightWheelSpeed(D9), leftWheelSpeed(D3);
+rightWheelSpeed = leftWheelSpeed = wheelSpeed;
+rightWheelSpeed.period(0.5);
+leftWheelSpeed.period(0.5);
+// ticker that movement instructions are sent to
 Ticker movement;
-char x,gx,y,gy;
-int i =0;
-void counter_clock();
-void normal_clock();
-void Move();
-enum Clock {twelve,six,nine,three};
-Clock face;
-int main() {
-    out1=out2=out3=out4=0;
-    ena=enb=.1;
-    ena.period(.5);
-    enb.period(.5);
-    gx=2;
-    gy=2;
-    x=0;
-    y=0;
-    face = twelve;
-    movement.attach(&Move, 1000ms);
-    while (1) {
-    }
+
+// x,y are the robot starting position
+// goalX,goalY are the goal position
+char x = 0, y = 0, goalX = 2, goalY = 2;
+
+// the robot's current ready state
+int i = 0;
+
+// function that rotates the robot counter clockwise
+void turnLeft();
+
+// function that rotates the robot clockwise
+void turnRight();
+
+// function that moves the robot forward
+void MoveFwd();
+
+// possible orientations of the robot
+enum cardinalDirection
+{
+    north,
+    south,
+    east,
+    west
+};
+
+// the robot's current orientation
+cardinalDirection face = north;
+
+
+int main()
+{
+    movement.attach(&MoveFwd, 1000ms);
 }
 
-void Move(){
-    if (gx != x || gy != y) {
-    if(face == twelve){
-    if (y < gy){out1=out3=1;out2=out4=0;y++;}
-    else if (y>gy){out2=out4=1;out1=out3=0;y--;}
-    else if (x < gx)movement.attach(&normal_clock,666ms);
-    else if (x > gx)movement.attach(&counter_clock,666ms);}
+void MoveFwd()
+{
+    // if the robot is not at the goal position
+    if (goalX != x || goalY != y)
+    {
+        if (face == north)
+        {
+            if (y < goalY)
+            {
+                rightFwdDrive = leftFwdDrive = 1;
+                leftBackDrive = leftBackDrive = 0;
+                y++;
+            }
+            else if (y > goalY)
+            {
+                leftBackDrive = leftBackDrive = 1;
+                rightFwdDrive = leftFwdDrive = 0;
+                y--;
+            }
+            else if (x < goalX)
+                movement.attach(&turnRight, 666ms);
+            else if (x > goalX)
+                movement.attach(&turnLeft, 666ms);
+        }
 
-    else if(face == six){
-    if (y < gy){out2=out4=1;out1=out3=0;y++;}
-    else if (y>gy){out1=out3=1;out2=out4=0;y--;}
-    else if (x < gx)movement.attach(&counter_clock,666ms);
-    else if (x > gx)movement.attach(&normal_clock,666ms);}
+        else if (face == south)
+        {
+            if (y < goalY)
+            {
+                leftBackDrive = leftBackDrive = 1;
+                rightFwdDrive = leftFwdDrive = 0;
+                y++;
+            }
+            else if (y > goalY)
+            {
+                rightFwdDrive = leftFwdDrive = 1;
+                leftBackDrive = leftBackDrive = 0;
+                y--;
+            }
+            else if (x < goalX)
+                movement.attach(&turnLeft, 666ms);
+            else if (x > goalX)
+                movement.attach(&turnRight, 666ms);
+        }
 
-    else if(face==three){
-    if (x < gx){out1=out3=1;out2=out4=0;x++;}
-    else if (x>gx){out2=out4=1;out1=out3=0;x--;}
-    else if (y < gy)movement.attach(&counter_clock,666ms);
-    else if (y > gy)movement.attach(&normal_clock,666ms);}
-   
-    else if(face==nine){
-    if (x < gx){out2=out4=1;out1=out3=0;x++;}
-    else if (x>gx){out1=out3=1;out2=out4=0;x--;}
-    else if (y < gy)movement.attach(&normal_clock,666ms);
-    else if (y > gy)movement.attach(&counter_clock,666ms);}
+        else if (face == east)
+        {
+            if (x < goalX)
+            {
+                rightFwdDrive = leftFwdDrive = 1;
+                leftBackDrive = leftBackDrive = 0;
+                x++;
+            }
+            else if (x > goalX)
+            {
+                leftBackDrive = leftBackDrive = 1;
+                rightFwdDrive = leftFwdDrive = 0;
+                x--;
+            }
+            else if (y < goalY)
+                movement.attach(&turnLeft, 666ms);
+            else if (y > goalY)
+                movement.attach(&turnRight, 666ms);
+        }
+
+        else if (face == west)
+        {
+            if (x < goalX)
+            {
+                leftBackDrive = leftBackDrive = 1;
+                rightFwdDrive = leftFwdDrive = 0;
+                x++;
+            }
+            else if (x > goalX)
+            {
+                rightFwdDrive = leftFwdDrive = 1;
+                leftBackDrive = leftBackDrive = 0;
+                x--;
+            }
+            else if (y < goalY)
+                movement.attach(&turnRight, 666ms);
+            else if (y > goalY)
+                movement.attach(&turnLeft, 666ms);
+        }
     }
-    else if(gx == x && gy == y){
-    out1=out2=out3=out4=0;
-     movement.detach();}
+    else if (goalX == x && goalY == y)
+    {
+        rightFwdDrive = leftBackDrive = leftFwdDrive = leftBackDrive = 0;
+        movement.detach();
+    }
 }
-void counter_clock(){
-    out1=out4=1;
-    out2=out3=0;    
-    if (i==0){
-        switch (face) {
-        case twelve: face = nine;break;
-        case six: face = three;break;
-        case nine: face = six;break;
-        case three: face = twelve;break;
+void turnLeft()
+{
+    rightFwdDrive = leftBackDrive = 1;
+    leftBackDrive = leftFwdDrive = 0;
+    if (i == 0)
+    {
+        switch (face)
+        {
+        case north:
+            face = west;
+            break;
+        case south:
+            face = east;
+            break;
+        case west:
+            face = south;
+            break;
+        case east:
+            face = north;
+            break;
         }
         i++;
     }
-    else{
+    else
+    {
         i = 0;
-        movement.attach(&Move,1000ms);
+        movement.attach(&MoveFwd, 1000ms);
     }
 }
-void normal_clock(){
-    out2=out3=1;
-    out1=out4=0;
-    if (i==0){
-        switch (face) {
-        case twelve: face = three;break;
-        case six: face = nine;break;
-        case nine: face = twelve;break;
-        case three: face = six;break;
+void turnRight()
+{
+    leftBackDrive = leftFwdDrive = 1;
+    rightFwdDrive = leftBackDrive = 0;
+    if (i == 0)
+    {
+        switch (face)
+        {
+        case north:
+            face = east;
+            break;
+        case south:
+            face = west;
+            break;
+        case west:
+            face = north;
+            break;
+        case east:
+            face = south;
+            break;
         }
         i++;
     }
-    else {
+    else
+    {
         i = 0;
-        movement.attach(&Move,1000ms);
-    }   
+        movement.attach(&MoveFwd, 1000ms);
+    }
 }
