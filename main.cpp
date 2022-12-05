d29a0bfe03a847a81eaba91a170688baab178e44
n9barton <92070419+n9barton@users.noreply.github.com>
2022-12-05 11:39:42 -0600

    added scanner
    added scanWall function, reformatted some of the other functions

--- main.cpp
+++ main.cpp
@@ -5,7 +5,14 @@
 const float wheelSpeed = .9;
 
 chrono::milliseconds turn = 200ms;
+chrono::milliseconds _180 = 400ms;
 chrono::milliseconds on = 420ms;
+
+//Trigger for scanner
+DigitalOut trig(D8);
+
+//input for scanner
+DigitalIn echo(D3);
 
 // digital outputs for the wheels
 DigitalOut rightFwdDrive(D5), rightBackDrive(D4), leftFwdDrive(D7),
@@ -13,16 +20,23 @@ DigitalOut rightFwdDrive(D5), rightBackDrive(D4), leftFwdDrive(D7),
 
 //Display for Y
 BusOut displayY(PC_3,PC_2,PH_1,PH_0,
-PC_15, PC_14,PC_13);
+PC_15,PC_14,PC_13);
 
 //Display for X 
-BusOut displayX();
+BusOut displayX(PC_4,PB_13,PB_14,PB_15,
+PB_1,PB_2,PB_12);
 
 // digital outputs for the Pulse Width Modulation controlling wheel speed
 PwmOut rightWheelSpeed(D9), leftWheelSpeed(D3);
 
 // ticker that movement instructions are sent to
 Ticker movement;
+
+// ticker that controls display
+Ticker display;
+
+//scanning for walls
+Ticker scan;
 
 // is there a wall in front of the robot
 bool wall = false;
@@ -36,7 +50,7 @@ int i = 0;
 
 
 // function that displays position that the robot is in
-void displayNode(int num, BusOut dis);
+void displayNode();
 
 
 // function that scans for walls that robot must avoid
@@ -64,6 +78,8 @@ int main() {
   leftWheelSpeed.period(0.1);
   rightFwdDrive = leftBackDrive = leftFwdDrive = rightBackDrive = 0;
   rightWheelSpeed = leftWheelSpeed = wheelSpeed;
+  display.attach(&displayNode,100ms);
+  scan.attach(&scanWall,100ms);
   movement.attach(&MoveFwd, on);
   while (1) {
   }
@@ -76,14 +92,12 @@ void MoveFwd() {
   // if the robot is not at the goal position
   if (goalX != x || goalY != y) {
     if (face == north) {
-      if (y < goalY) {
+      if (y < goalY && wall == false) {
         rightFwdDrive = leftFwdDrive = 1;
         rightBackDrive = leftBackDrive = 0;
         y++;
       } else if (y > goalY) {
-        rightBackDrive = leftBackDrive = 1;
-        rightFwdDrive = leftFwdDrive = 0;
-        y--;
+        movement.attach(&turnRight,_180);
       } else if (x < goalX)
         movement.attach(&turnRight, turn);
       else if (x > goalX)
@@ -91,14 +105,12 @@ void MoveFwd() {
     }
 
     else if (face == south) {
-      if (y < goalY) {
+      if (y < goalY && wall == false) {
         rightBackDrive = leftBackDrive = 1;
         rightFwdDrive = leftFwdDrive = 0;
-        y++;
-      } else if (y > goalY) {
-        rightFwdDrive = leftFwdDrive = 1;
-        rightBackDrive = leftBackDrive = 0;
         y--;
+      } else if (y > goalY) {
+        movement.attach(&turnRight,_180);
       } else if (x < goalX)
         movement.attach(&turnLeft, turn);
       else if (x > goalX)
@@ -106,14 +118,12 @@ void MoveFwd() {
     }
 
     else if (face == east) {
-      if (x < goalX) {
+      if (x < goalX && wall == false) {
         rightFwdDrive = leftFwdDrive = 0;
         rightBackDrive = leftBackDrive = 1;
         x++;
       } else if (x > goalX) {
-        rightBackDrive = leftBackDrive = 0;
-        rightFwdDrive = leftFwdDrive = 1;
-        x--;
+        movement.attach(&turnRight,_180);
       } else if (y < goalY)
         movement.attach(&turnLeft, turn);
       else if (y > goalY)
@@ -121,14 +131,12 @@ void MoveFwd() {
     }
 
     else if (face == west) {
-      if (x < goalX) {
+      if (x < goalX && wall == false) {
         rightBackDrive = leftBackDrive = 0;
         rightFwdDrive = leftFwdDrive = 1;
         x++;
       } else if (x > goalX) {
-        rightFwdDrive = leftFwdDrive = 0;
-        rightBackDrive = leftBackDrive = 1;
-        x--;
+        movement.attach(&turnRight,_180);
       } else if (y < goalY)
         movement.attach(&turnRight, turn);
       else if (y > goalY)
@@ -170,6 +178,7 @@ void turnLeft() {
   } 
   else {
     i = 0;
+    wall = false;
     rightBackDrive = rightFwdDrive = leftBackDrive = leftFwdDrive =0;
     movement.attach(&MoveFwd, on);
   }
@@ -190,20 +199,39 @@ void turnRight() {
   } 
   else {
     i = 0;
+    wall = false;
     rightBackDrive = rightFwdDrive = leftBackDrive = leftFwdDrive =0;
     movement.attach(&MoveFwd, on);
   }
 }
 
-void displayNode(int num, BusOut dis){
-    switch (num) {
-        case 0:dis = 0;break;
-        case 1:dis = 1;break;
-        case 2:dis = 2;break;
-        case 3:dis = 3;break;
-        case 4:dis = 4;break;
-        case 5:dis = 5;break;
-        case 6:dis = 6;break;
-        default:dis =12;break;
+void displayNode(){
+    switch (x) {
+        case 0:displayX = 0;break;
+        case 1:displayX = 1;break;
+        case 2:displayX = 2;break;
+        case 3:displayX = 3;break;
+        case 4:displayX = 4;break;
+        case 5:displayX = 5;break;
+        case 6:displayX = 6;break;
+        default:displayX=12;break;
     }
+    switch (y) {
+        case 0:displayY = 0;break;
+        case 1:displayY = 1;break;
+        case 2:displayY = 2;break;
+        case 3:displayY = 3;break;
+        case 4:displayY = 4;break;
+        case 5:displayY = 5;break;
+        case 6:displayY = 6;break;
+        default:displayY=12;break;
+    }
+}
+
+void scanWall(){
+    trig = 1;
+    wait_us(10);
+    trig = 0;
+    wait_us(1000);
+    if(echo.read() == 1) wall = true;
 }
